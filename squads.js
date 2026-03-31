dba1a168f10bd66a10b50f3186c87a8f3350442b
wiztemple <wiztemple7@gmail.com>
2026-03-31 22:43:25 +0100

    Add squads

---
+++ squads.js
@@ -0,0 +1,35 @@
+window.SQUADS = {
+  Mexico: [
+    { n: 1, p: 'GK', nm: 'Guillermo Ochoa', cap: true, c: 'Salernitana' },
+    { n: 13, p: 'GK', nm: 'Luis Malagón', c: 'América' },
+    { n: 2, p: 'DF', nm: 'Jorge Sánchez', c: 'Porto' },
+    { n: 3, p: 'DF', nm: 'César Montes', c: 'Almería' },
+    { n: 4, p: 'MF', nm: 'Edson Álvarez', c: 'West Ham' },
+    { n: 7, p: 'MF', nm: 'Luis Romo', c: 'Monterrey' },
+    { n: 9, p: 'FW', nm: 'Raúl Jiménez', c: 'Fulham' },
+    { n: 22, p: 'FW', nm: 'Hirving Lozano', c: 'PSV Eindhoven' }
+  ],
+  USA: [
+    { n: 1, p: 'GK', nm: 'Matt Turner', c: 'Nottingham Forest' },
+    { n: 4, p: 'DF', nm: 'Tyler Adams', cap: true, c: 'Bournemouth' },
+    { n: 8, p: 'MF', nm: 'Weston McKennie', c: 'Juventus' },
+    { n: 10, p: 'FW', nm: 'Christian Pulisic', c: 'AC Milan' },
+    { n: 21, p: 'FW', nm: 'Tim Weah', c: 'Juventus' }
+  ],
+  Canada: [
+    { n: 18, p: 'GK', nm: 'Milan Borjan', cap: true, c: 'Slovan Bratislava' },
+    { n: 19, p: 'DF', nm: 'Alphonso Davies', c: 'Bayern Munich' },
+    { n: 7, p: 'MF', nm: 'Stephen Eustáquio', c: 'Porto' },
+    { n: 20, p: 'FW', nm: 'Jonathan David', c: 'Lille' }
+  ],
+  Brazil: [
+    { n: 1, p: 'GK', nm: 'Alisson', c: 'Liverpool' },
+    { n: 2, p: 'DF', nm: 'Danilo', cap: true, c: 'Juventus' },
+    { n: 4, p: 'DF', nm: 'Marquinhos', c: 'PSG' },
+    { n: 5, p: 'MF', nm: 'Casemiro', c: 'Man Utd' },
+    { n: 8, p: 'MF', nm: 'Bruno Guimarães', c: 'Newcastle' },
+    { n: 7, p: 'FW', nm: 'Vinícius Júnior', c: 'Real Madrid' },
+    { n: 9, p: 'FW', nm: 'Richarlison', c: 'Tottenham' },
+    { n: 10, p: 'FW', nm: 'Neymar', c: 'Al Hilal' }
+  ]
+};
